e199ccd4ba7caf62ce758ef5103dee7ca81b3f0b
Lusha404 <Lusha404@yandex.ru>
2023-11-29 16:22:08 +0300

    v1

---
+++ internal/app/grpc/app.go
@@ -0,0 +1,62 @@
+package grpcapp
+
+import (
+	"fmt"
+	"google.golang.org/grpc"
+	"log/slog"
+	"net"
+	authgrpc "sso/internal/grpc/auth"
+)
+
+type App struct {
+	log        *slog.Logger
+	gRPCServer *grpc.Server
+	port       int
+}
+
+func New(log *slog.Logger, authService authgrpc.Auth, port int) *App {
+	gRPCServer := grpc.NewServer()
+	authgrpc.Register(gRPCServer, authService)
+	return &App{
+		log:        log,
+		gRPCServer: gRPCServer,
+		port:       port,
+	}
+}
+
+func (a *App) MustRun() {
+	if err := a.Run(); err != nil {
+		panic(err)
+	}
+}
+
+func (a *App) Run() error {
+	const op = "grpcapp.Run"
+
+	log := a.log.With(
+		slog.String("op", op),
+		slog.Int("port", a.port),
+	)
+
+	log.Info("starting gRPC server")
+
+	l, err := net.Listen("tcp", fmt.Sprintf(":%v", a.port))
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+	log.Info("grpc server is running", slog.String("addr", l.Addr().String()))
+
+	if err := a.gRPCServer.Serve(l); err != nil {
+		return fmt.Errorf("%s: %s", op, err)
+	}
+	return nil
+}
+
+func (a *App) Stop() {
+	const op = "grpc.Stop"
+
+	a.log.With(slog.String("op", op)).
+		Info("stopping gRPC server", slog.Int("port", a.port))
+
+	a.gRPCServer.GracefulStop()
+}
